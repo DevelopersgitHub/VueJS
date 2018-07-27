a363253f98132f22725bfdfaadbe28d4014a0ebe
VLADISLAV GUSHCHIN <vlad-nighthunter@mail.ru>
2018-07-27 23:14:32 +0300

    minus eslint

--- src/components/Class.vue
+++ src/components/Class.vue
@@ -1,35 +1,37 @@
 <template>
-  <div id="class">
-    <div class="region" :class="{'active' : isActive}" v-on:click="enabled()"></div>
-    <div class="region"></div>
-  </div>
+    <div id="class">
+        region
+        <div class="region" :class="{'active' : isActive}" v-on:click="enabled()"></div>
+        <div class="region"></div>
+    </div>
 </template>
 
 <script>
     export default {
         name: "Class",
-      date(){
-          return{
-            isActive: false
-          }
-      },
-      methods: {
-        enabled(){
-         this.isActive =!this.isActive;
+        data(){
+            return {
+                isActive: false
+            }
+        },
+        methods: {
+            enabled(){
+                this.isActive = !this.isActive;
+            }
         }
-      }
     }
 </script>
 
 <style scoped>
-  .region{
-    background-color: #999;
-    width: 100px;
-    height: 100px;
-    display: inline-block;
-    margin: 10px;
-  }
-  .active{
-    background-color: red;
-  }
+    .region {
+        background-color: #999;
+        width: 100px;
+        height: 100px;
+        display: inline-block;
+        margin: 10px;
+    }
+
+    .active {
+        background-color: red;
+    }
 </style>
