24fb83144a4e76def1c8aeb9fb896baa55814092
Mihail <mikhailov.miail@yandex.ru>
2018-07-28 01:01:24 +0300

    'f'

--- src/components/Class.vue
+++ src/components/Class.vue
@@ -1,39 +1,57 @@
 <template>
     <div id="class">
-        <div class="region" :class="{'active' : isActive}" v-on:click="enabled()">
-            region
-
-        </div>
-        <div class="region"></div>
+        <div class="region" v-bind:class="classObj"></div>
+        <div class="region" v-bind:class="{bounded : isBounded}"></div>
+        <button v-on:click="enabled()"></button>
+        <button v-on:click="enabledB()"></button>
     </div>
 </template>
 
 <script>
-    export default {
-        name: "Class",
-        data(){
-            return {
-                isActive: false
-            }
-        },
-        methods: {
-            enabled(){
-                this.isActive = !this.isActive;
-            }
+  export default {
+    name: "Class",
+    data() {
+      return {
+        isActive: false,
+        isBounded: false,
+        classObj: {
+          active: this.isActive,
+          bounded: this.isBounded
         }
+      }
+    },
+    methods: {
+      enabled() {
+        this.isActive = !this.isActive;
+      },
+      enabledB() {
+        this.isBounded = !this.isBounded;
     }
+    },
+    /*computed: {
+      classObj(){
+        return {
+          active: this.isActive,
+          bounded: this.isBounded
+      }
+      }
+    }*/
+  }
 </script>
 
 <style scoped>
-    .region {
-        background-color: #999;
-        width: 100px;
-        height: 100px;
-        display: inline-block;
-        margin: 10px;
-    }
+  .region {
+    background-color: #999;
+    width: 100px;
+    height: 100px;
+    display: inline-block;
+    margin: 10px;
+  }
 
-    .active {
-        background-color: red;
-    }
+  .active {
+    background-color: red;
+  }
+  .bounded {
+    border: 2px solid green;
+  }
 </style>
