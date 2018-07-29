3ccbd4a632d51cf1c4c5c2363fcd87fc56ef7a36
Mihail <mikhailov.miail@yandex.ru>
2018-07-29 21:16:51 +0300

    p

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,32 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <p class="strike">Welcome</p>
-    <ifVue/>
+    <p>Welcome</p>
+    <forVue/>
     <router-view/>
   </div>
 </template>
 <script>
-  import ifVue from './components/ifVue';
-export default {
-   name: 'App',
-   components: { ifVue }
-}
+  import forVue from './components/forVue';
+  export default {
+    name: 'App',
+    components: {forVue}
+  }
 </script>
 
 <style>
-#app {
- font-family: 'Avenir', Helvetica, Arial, sans-serif;
- -webkit-font-smoothing: antialiased;
- -moz-osx-font-smoothing: grayscale;
- text-align: center;
- color: #2c3e50;
- margin-top: 20px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+    margin-left: 100px;
+  }
 
-body{
- background: url('./assets/background.jpg'), no-repeat;
- font-family: Helvetica, sans-serif;
- font-size: 18px;
- color: #272727;
-}
-.strike{
-  color: #aaa;
-}
+  body {
+    background: url('./assets/background.jpg'), no-repeat;
+    font-family: Helvetica, sans-serif;
+    font-size: 18px;
+    color: #272727;
+  }
 </style>
---
+++ src/components/forVue.vue
@@ -0,0 +1,91 @@
+<template>
+  <div>
+    <p>
+      <input type="text" v-model="newPhone"/>
+      <button v-on:click="phones.push(newPhone)">Добавить</button>
+    </p>
+    <ul>
+      <li v-for="(phone, index) in phones">
+        <p>{{phone.name }} {{phone.cost }}
+          <button v-on:click="phones.splice(index, 1)">Удалить</button>
+        </p>
+      </li>
+    </ul>
+    <!--<input type="text" v-model="newPhone"/>
+   <button v-on:click="phones.push(newPhone)">Добавить</button>
+   <ul>
+     <li v-for="(phone, index) in phones">
+       <p>{{ index + 1 }} . Name: {{ phone.name }}</p>
+       <p>Cost: {{ phone.cost }}</p>
+     </li>
+     <p> {{ phone }} <button v-on:click="phones.splice(index, 1)">Удалить</button></p>
+   </ul>
+   <li v-for="(phone, index) in phones"> {{ index + 1 }}. {{ phone.name }}</li>
+   <ul>
+     <li v-for="user in users">
+       <p v-for="(value, key) in user">{{ key }} : {{ value }}</p>
+     </li>
+   </ul>
+   <ul>
+     <template v-for="user in users">
+       <li>Name: {{ user.name }}</li>
+       <li>newUser: {{ user.newUser }}</li>
+     </template>
+   </ul>
+   <span v-for="n in 100"> {{ n }}</span>-->
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "forVue",
+    data() {
+      return {
+        newPhone: {
+          name: '',
+          cost: ''
+        },
+        phones: [
+          {
+            name: 'Iphone6',
+            cost: '100$'
+          },
+          {
+            name: 'Iphone7',
+            cost: '120$'
+          },
+          {
+            name: 'Iphone8',
+            cost: '160$'
+          },
+          {
+            name: 'IphoneX',
+            cost: '200$'
+          }
+        ],
+        newPhones: ['Iphone6', 'Iphone7', 'Iphone8', 'IphoneX',],
+        users: [
+          {
+            name: 'Mikhail',
+            newUser: 'true'
+          },
+          {
+            name: 'Vlad',
+            newUser: 'false'
+          },
+          {
+            name: 'Robert',
+            newUser: 'false'
+          }
+        ]
+      }
+    },
+  }
+</script>
+
+<style scoped>
+  ul {
+    float: top;
+  }
+</style>
